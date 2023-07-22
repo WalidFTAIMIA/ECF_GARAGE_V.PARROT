17fe77a643c0682f1de2d1ae34d74436e316e15b
Walid <walid.ftaymia@gmail.com>
2023-07-22 13:08:17 +0200

    update syntaxe

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -22,7 +22,7 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(type: 'json', length: 180)]
+    #[ORM\Column(type: "json")]
     private array $roles = [];
 
     /**
